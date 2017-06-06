8ca1d7c491fc8ce9b04b1af39dcacb0976559b99
skykeeper0 <trungdinh0429@gmail.com>
2017-06-05 21:54:58 -0700

    add mockAsyncStorage.js

--- app/utils/storageMethods.test.js
+++ app/utils/storageMethods.test.js
@@ -1,5 +1,60 @@
 import { getAsyncStorage } from './storageMethods';
 
-test('get data from storage', () => {
-  expect(getAsyncStorage()).toBe(3)
+import MockStorage from './MockStorage';
+
+const storageCache = {};
+const AsyncStorage = new MockStorage(storageCache);
+
+jest.setMock('AsyncStorage', AsyncStorage)
+
+const seeded_expenses = {
+        '2017': {
+            '01': {
+                budget: 500,
+                expenses: [
+                    {
+                        amount: '4',
+                        category: 'Coffee',
+                        date: 'Jan 12, 2017',
+                        description: 'Latte @ Coffeeshop'
+                    },{
+                        amount: '1.50',
+                        category: 'Books',
+                        date: 'Jan 17, 2017',
+                        description: 'Sunday Newspaper'
+                    }
+                ]
+            }
+        }
+    }
+
+it('Mock Async Storage working', async () => {
+  await AsyncStorage.setItem('myKey','myValue')
+  const value = await AsyncStorage.getItem('myKey')
+  expect(value).toBe('myValue')
 })
+
+describe('getAsyncStorage', () => {
+  // add seeededData into Storage and check the data it got back
+  const expenses = {'a':'b'}
+
+  beforeEach(async () => {
+    await AsyncStorage.setItem('test', JSON.stringify(expenses))
+  })
+
+  it('should be able to load data from AsyncStorage', async () => {
+    const data = await getAsyncStorage()
+    expect(data).toEqual(expenses)
+  })
+})
+
+// xdescribe('setAsyncStorage', () => {
+
+//  xit('should be able set data into Storage when storage is empty', () => {
+
+//   })
+
+//  xit('should be able set data into Storage when storage isnt empty', () => {
+
+//   })
+// })
