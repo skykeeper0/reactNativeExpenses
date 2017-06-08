4ed5f1bba21bbde30ef85db84396cfb30e7ab224
skykeeper0 <trungdinh0429@gmail.com>
2017-06-07 22:01:41 -0700

    add testData and dateMethods.test.js

---
+++ app/utils/__test__/dateMethods.test.js
@@ -0,0 +1,9 @@
+import seeded_expenses from './testData.js'
+import MockDate from 'mockdate'
+
+// set the current time to 1/1/2017
+MockDate.set('1/1/2017');
+
+
+
+MockDate.reset()
---
+++ app/utils/__test__/testData.js
@@ -0,0 +1,20 @@
+const seeded_expenses = {
+        '2017': {
+            '01': {
+                budget: 500,
+                expenses: [
+                    {
+                        amount: '4',
+                        category: 'Coffee',
+                        date: 'Jan 12, 2017',
+                        description: 'Latte @ Coffeeshop'
+                    },{
+                        amount: '1.50',
+                        category: 'Books',
+                        date: 'Jan 17, 2017',
+                        description: 'Sunday Newspaper'
+                    }
+                ]
+            }
+        }
+    }
